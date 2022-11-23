d3b565396a4f058fcb4c6e37bd14581ec622fff0
ManuelFoidl <manufoidl@tsn.at>
2022-11-23 10:06:57 +0100

    Daten einfügen

--- Datenpersistenz/JDBC_Intro/jdbcdemo/jdbcdemo/src/main/java/at/itkolleg/JDBCDemo.java
+++ Datenpersistenz/JDBC_Intro/jdbcdemo/jdbcdemo/src/main/java/at/itkolleg/JDBCDemo.java
@@ -8,7 +8,34 @@ public class JDBCDemo {
         System.out.println("JDBC Demo!");
         //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Manuel Foidl', 'manue@foidl'), (NULL, 'max muster', 'max@muster');
         selectAlldemo();
+        insertStudentDemo();
+        selectAlldemo();
     }
+    public static void insertStudentDemo(){
+        System.out.println("Insert Demo mit JDBC");
+        String connectionURL = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pw = "";
+        try (Connection conn = DriverManager.getConnection(connectionURL,"root", "")){
+            System.out.println("Verbindung zur DB hergestellt!");
+            //SQL Statement vorbereiten
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, ?, ?)");
+            try {
+                preparedStatement.setString(1, "Peter Zeck");
+                preparedStatement.setString(2, "p.zeck@gmail.com");
+                int rowAffected = preparedStatement.executeUpdate();
+                System.out.println("Datensätze eingefügt: " + rowAffected);
+
+            }catch (SQLException ex) {
+                System.out.println("Fehler im der SQL-INSERT Statement: " + ex.getMessage());
+            }
+        }catch (SQLException e)
+        {
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+
     public static void selectAlldemo(){
         System.out.println("Select Demo mit JDBC");
         String sqlSelectAllPersons = "SELECT * FROM `student`";
