bcbffa02018f0885ebab3e7bff71f4417ce60545
ManuelFoidl <manufoidl@tsn.at>
2022-11-26 09:39:40 +0100

    JDBC Demo 2 Insert in Tabelle Hobbies

--- Datenpersistenz/JDBC_Intro/jdbcdemo/jdbcdemo/src/main/java/at/itkolleg/JDBCDemo2.java
+++ Datenpersistenz/JDBC_Intro/jdbcdemo/jdbcdemo/src/main/java/at/itkolleg/JDBCDemo2.java
@@ -1,39 +1,66 @@
 package at.itkolleg;
 
 import java.math.BigDecimal;
-import java.nio.channels.FileLock;
 import java.sql.*;
-import java.util.Formatter;
 
 public class JDBCDemo2 {
     public static void main(String[] args) {
         selectAll();
+        insertHobby("Schwimmen", new BigDecimal(0));
+        selectAll();
+
 
     }
 
-    public static void selectAll(){
+    public static void selectAll() {
         String connectionURL = "jdbc:mysql://localhost:3306/jdbcdemo";
         String user = "root";
         String pw = "";
 
-        try(Connection conn = DriverManager.getConnection(connectionURL, user, pw)){
+        try (Connection conn = DriverManager.getConnection(connectionURL, user, pw)) {
             String sqlstatement = "SELECT * FROM hobbies";
             PreparedStatement preparedStatement = conn.prepareStatement(sqlstatement);
 
             ResultSet rs = preparedStatement.executeQuery();
 
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String bezeichnung = rs.getString("bezeichnung");
                 BigDecimal betrag = rs.getBigDecimal("beitrag");
                 System.out.printf("Hobby: ID %d, Bezeichnung: %s, Betrag: %s %n", id, bezeichnung, betrag.toString());
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Fehler " + e.getMessage());
 
         }
 
+    }
+
+    public static void insertHobby(String bezeichnung, BigDecimal beitrag) {
+        String connectionURL = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pw = "";
+
+        try (Connection conn = DriverManager.getConnection(connectionURL, user, pw)) {
+            String sqlstatement = "INSERT INTO `hobbies` (`id`, `bezeichnung`, `beitrag`) VALUES (NULL, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlstatement);
+
+            try {
+                preparedStatement.setString(1, bezeichnung);
+                preparedStatement.setBigDecimal(2, beitrag);
+                int affectedRows = preparedStatement.executeUpdate();
+                System.out.println("Datensätze hinzugefügt: " + affectedRows);
+
+            } catch (SQLException e) {
+                System.out.println("Fehler beim Insert " + e.getMessage());
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Fehler " + e.getMessage());
+
+        }
     }
 }
 
