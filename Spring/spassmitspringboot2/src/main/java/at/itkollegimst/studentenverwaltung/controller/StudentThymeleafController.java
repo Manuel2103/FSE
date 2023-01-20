5ef31bc6f0dd46533f9338cf72162c11a23b65c5
ManuelFoidl <manufoidl@tsn.at>
2023-01-20 10:14:00 +0100

    JavaScript Frontend Thymeleaf Teil 3

--- Spring/spassmitspringboot2/src/main/java/at/itkollegimst/studentenverwaltung/controller/StudentThymeleafController.java
+++ Spring/spassmitspringboot2/src/main/java/at/itkollegimst/studentenverwaltung/controller/StudentThymeleafController.java
@@ -1,11 +1,13 @@
 package at.itkollegimst.studentenverwaltung.controller;
 
 import at.itkollegimst.studentenverwaltung.domain.Student;
+import at.itkollegimst.studentenverwaltung.exceptions.StudentNichtGefunden;
 import at.itkollegimst.studentenverwaltung.services.StudentenService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,6 +43,34 @@ public class StudentThymeleafController {
         }else{
             this.studentService.studentEinfuegen(student);
             return "redirect:/web/v1/studenten";
+        }
+    }
+
+    @GetMapping("/update/{id}")
+    public String studentUpdatenFormular(@PathVariable Long id, Model model){
+
+        try{
+            Student student = this.studentService.studentMitId(id);
+            model.addAttribute("student", student);
+            return "studentenupdaten";
+
+        }catch (StudentNichtGefunden studentNichtGefunden){
+            return "redirect:web/v1/studenten";
+        }
+    }
+
+    @PostMapping("/update")
+    public String stuentUpdaten(@Valid Student student, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "studentenupdaten";
+        }else{
+            try{
+                this.studentService.studentUpdaten(student);
+                return "redirect:/web/v1/studenten";
+
+            }catch (StudentNichtGefunden studentNichtGefunden){
+                return "redirect:/web/v1/studenten";
+            }
         }
     }
 
